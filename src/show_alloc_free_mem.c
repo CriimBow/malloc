656aca2c0d9b76b637946736084cf433e885a56b
ggranjon <guillaume.granjondelepiney@laposte.net>
2019-03-07 11:48:18 +0100

    legere correction

--- src/show_alloc_free_mem.c
+++ src/show_alloc_free_mem.c
@@ -40,7 +40,7 @@ void						show_alloc_free_mem(void)
 
 	total = 0;
 	thread_protection(0);
-	if (!(init_n_ret(0)) || !(init_n_ret(0)->tiny) || !(init_n_ret(0)->small))
+	if (!(init_n_ret(0)))
 		return ;
 	ft_putstr("TINY : 0x");
 	print_hex((long)(init_n_ret(0)->tiny));
